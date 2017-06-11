93e165d599427e75557e36e255d818edb54eb598
Luis Henrique Mulinari <luis.mulinari@gmail.com>
2017-06-11 11:42:10 +0200

    Add newrelic middleware

--- ranger_metrics/newrelic.go
+++ ranger_metrics/newrelic.go
@@ -1,11 +1,37 @@
 package ranger_metrics
 
-import newrelic "github.com/newrelic/go-agent"
+import (
+	newrelic "github.com/newrelic/go-agent"
+	"net/http"
+	"fmt"
+)
 
-// InitNewRelic ...
-func InitNewRelic(appName string, license string) (newrelic.Application, error) {
-	return newrelic.NewApplication(newrelic.NewConfig(
+type NewRelic struct {
+	Application newrelic.Application
+}
+
+func NewNewRelic(appName string, license string) *NewRelic {
+	app, err := newrelic.NewApplication(newrelic.NewConfig(
 		appName,
 		license),
 	)
+
+	if err != nil {
+		panic(fmt.Errorf("NewRelic error: %s", err))
+	}
+
+	return &NewRelic{
+		Application: app,
+	}
+}
+
+func (newRelic *NewRelic) Middleware(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		txn := newRelic.Application.StartTransaction(r.URL.Path, w, r)
+		defer txn.End()
+
+		next.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(fn)
 }
